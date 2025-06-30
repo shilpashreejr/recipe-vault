98ce044db07bdaa39117c73c5251ea045c9fa6e9
Shilpa Shree Pulakunta <shilpashreepulakunta@Shilpas-MacBook-Pro.local>
2025-06-29 23:29:48 -0700

    Remove scroll indicator from hero section on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,13 +96,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <div className="w-6 h-10 border-2 border-primary/50 rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-primary rounded-full mt-2 animate-pulse"></div>
-          </div>
-        </div>
       </section>
 
       {/* Data Extraction Sources Section */}
